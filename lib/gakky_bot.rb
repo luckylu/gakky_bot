a57108a9167db0ccc79aff3344ee5fd23efa8922
Michael <kobelu1990@gmail.com>
2016-09-29 22:34:38 +0800

    add wordpress api

--- lib/gakky_bot.rb
+++ lib/gakky_bot.rb
@@ -8,13 +8,17 @@ require 'gakky_bot/version'
 require 'gakky_bot/twitter/oauth'
 require 'gakky_bot/twitter/api'
 require 'gakky_bot/model/token'
+require 'gakky_bot/wordpress/api'
 
 ActiveRecord::Base.establish_connection(YAML.load_file(File.expand_path("../gakky_bot/config/database.yml", __FILE__)))
 
 module GakkyBot
   class << self
     def user_timeline
-      puts Twitter::Api.user_timeline(Twitter::Oauth.bearer_token, 'lespros_sec1')
+      user_timeline =  Twitter::Api.user_timeline(Twitter::Oauth.bearer_token, 'lespros_sec1')
+      JSON.parse(user_timeline.body).each do |tweet|
+        Wordpress::Api.post(tweet) if tweet['entities']['hashtags'].map{|x| x['text']}.include?("新垣結衣")
+      end
     end
   end
 end
---
+++ lib/gakky_bot/wordpress/api.rb
@@ -0,0 +1,11 @@
+module GakkyBot
+  module Wordpress
+    class Api
+      class << self
+        def post(tweet)
+          puts tweet
+        end
+      end
+    end
+  end
+end
